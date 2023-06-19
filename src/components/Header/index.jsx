0f4e392c2edc24bc7b8fc6111d03861ec5526c13
Seidali001 <seitmambetovseidali@gmail.com>
2023-06-19 13:57:15 +0100

    add new position of header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -16,7 +16,7 @@ import MoreIcon from '@mui/icons-material/MoreVert';
 import {Search} from "@mui/icons-material";
 import {useDispatch, useSelector} from "react-redux";
 import {logout, selectIsAuth} from "../../redux/slices/auth";
-import {ThemeProvider, Tooltip} from "@mui/material";
+import {PropTypes, Slide, ThemeProvider, Tooltip, useScrollTrigger} from "@mui/material";
 import Button from "@mui/material/Button";
 import LogoutIcon from '@mui/icons-material/Logout';
 import {Link} from "react-router-dom";
@@ -27,7 +27,7 @@ import HomeIcon from '@mui/icons-material/Home';
 import Avatar from "@mui/material/Avatar";
 
 
-export const Header = () => {
+export const Header = (props) => {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -186,9 +186,35 @@ export const Header = () => {
         },
     });
 
+    function HideOnScroll(props) {
+        const { children, window } = props;
+        // Note that you normally won't need to set the window ref as useScrollTrigger
+        // will default to window.
+        // This is only being set here because the demo is in an iframe.
+        const trigger = useScrollTrigger({
+            target: window ? window() : undefined,
+        });
+
+        return (
+            <Slide appear={false} direction="down" in={!trigger}>
+                {children}
+            </Slide>
+        );
+    }
+
+    HideOnScroll.propTypes = {
+        children: PropTypes.element.isRequired,
+        /**
+         * Injected by the documentation to work in an iframe.
+         * You won't need it on your project.
+         */
+        window: PropTypes.func,
+    };
+
     return (
         <Box sx={{flexGrow: 1}}>
             <ThemeProvider theme={darkTheme}>
+                <HideOnScroll {...props}>
                 <AppBar position="static" >
                     <Toolbar>
                         <IconButton
@@ -270,6 +296,7 @@ export const Header = () => {
                         </Box>
                     </Toolbar>
                 </AppBar>
+                </HideOnScroll>
             </ThemeProvider>
             {renderMobileMenu}
             {renderMenu}
