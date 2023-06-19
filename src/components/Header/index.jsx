fa09ce55d3281ab2241b1ed63cce3338640600b6
Seidali001 <seitmambetovseidali@gmail.com>
2023-06-19 14:02:56 +0100

    back in default header position

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -186,35 +186,10 @@ export const Header = (props) => {
         },
     });
 
-    function HideOnScroll(props) {
-        const { children, window } = props;
-        // Note that you normally won't need to set the window ref as useScrollTrigger
-        // will default to window.
-        // This is only being set here because the demo is in an iframe.
-        const trigger = useScrollTrigger({
-            target: window ? window() : undefined,
-        });
-
-        return (
-            <Slide appear={false} direction="down" in={!trigger}>
-                {children}
-            </Slide>
-        );
-    }
-
-    HideOnScroll.propTypes = {
-        children: PropTypes.element.isRequired,
-        /**
-         * Injected by the documentation to work in an iframe.
-         * You won't need it on your project.
-         */
-        window: PropTypes.func,
-    };
 
     return (
         <Box sx={{flexGrow: 1}}>
             <ThemeProvider theme={darkTheme}>
-                <HideOnScroll {...props}>
                 <AppBar position="static" >
                     <Toolbar>
                         <IconButton
@@ -296,7 +271,6 @@ export const Header = (props) => {
                         </Box>
                     </Toolbar>
                 </AppBar>
-                </HideOnScroll>
             </ThemeProvider>
             {renderMobileMenu}
             {renderMenu}
