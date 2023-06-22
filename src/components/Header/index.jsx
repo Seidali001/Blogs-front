8b3f1b86959b4c422e095ec1fa063116a4249de8
Seidali001 <seitmambetovseidali@gmail.com>
2023-06-22 20:11:23 +0100

    changed input effect

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -61,13 +61,19 @@ export const Header = (props) => {
         },
     }));
 
-    const StyledInputBase = styled(InputBase)(({theme}) => ({
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
         color: 'inherit',
         '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
             transition: theme.transitions.create('width'),
             width: '100%',
-            [theme.breakpoints.up('md')]: {
-                width: '20ch',
+            [theme.breakpoints.up('sm')]: {
+                width: '12ch',
+                '&:focus': {
+                    width: '20ch',
+                },
             },
         },
     }));
